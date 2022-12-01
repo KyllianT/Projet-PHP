e11e010d72740a15d4d5e16d8fa0b79846c299ea
Airtox <117281228+KyllianT@users.noreply.github.com>
2022-12-01 17:45:20 +0100

    Delete Projet2-php-main directory

--- Projet2-php-main/Action/inscription.php
+++
@@ -1,8 +0,0 @@
-<?php require_once "config.php"; ?>
-<form method="post" action="signup.php">
-    <input type='text' name='firstName'/>
-    <input type='email' name='email' />
-    <input type='password' name='password' />
-    <input type='text' name='login' />
-    <input type='submit' value='Me connecter' />
-</form>
--- Projet2-php-main/security/testadmin.php
+++
@@ -1,6 +0,0 @@
-<?php require_once "config.php"; ?>
-<?php 
-if (!isset($_SESSION[$user] ) || $_SESSION[$user][$admin] == 0 )
-    header('Location:index.php');//on le redirige sur la page d'accueil du site !
-
-?>
